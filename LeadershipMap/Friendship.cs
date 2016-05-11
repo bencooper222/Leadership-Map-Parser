c666b88323dc7937c8f8b1884529fd5e2fb8cc12
bencooper222 <bencooper222@gmail.com>
2016-05-11 15:02:41 -0500

    Updated friendship class to work

--- LeadershipMap/Friendship.cs
+++ LeadershipMap/Friendship.cs
@@ -4,6 +4,40 @@ namespace LeadershipMap
 {
     public class Friendship
     {
-        public List<Leader> Members { get; set; }
+        public Leader[] friends { get; set; }
+        private bool crowdSourced;
+        private List<double> Ratings;
+        public double friendshipStrength { get; }
+        
+
+        public Friendship(Leader friend1, Leader friend2, bool friend1RatingExist, bool friend2RatingExist, params double[] ratings)
+        {
+            crowdSourced = (friend1RatingExist && friend1RatingExist) ? true : false; //if both friends responded, then it counts as crowdsourced.
+
+            friends = new Leader[2]; // make the friends data property
+            friends[0] = friend1;
+            friends[1] = friend2;
+            
+            foreach(double d in ratings)
+            {
+                Ratings.Add(d);
+            }
+
+            friendshipStrength = Average(Ratings);
+            
+         }
+
+        private double Average(List<double> doubles)
+        {
+            double sum = 0;
+
+            foreach(double d in doubles)
+            {
+                sum += d;
+            }
+
+            return sum / doubles.Count;
+        }
+
     }
 }
