12b1edcf09eb3ad25ae310066acfe87c4dec0869
Yugan Sakthi <ysakthi@imsa.edu>
2016-08-09 10:11:00 -0500

    Update xmlListformatter.py

--- Python/xmlListformatter.py
+++ Python/xmlListformatter.py
@@ -2,7 +2,8 @@ import xml.etree.ElementTree as et
 
 path = 'C:/Users/Yugan/Documents/GitHub/Leadership-Map-Website/Data/'
 et.register_namespace('', 'http://www.gexf.net/1.3')
-tree = et.parse(path + 'no name no zeros.gexf')
+et.register_namespace('viz', "http://www.gexf.net/1.3/viz") 
+tree = et.parse(path + 'no names no zeros.gexf')
 root = tree.getroot()
 
 for n in range(len(root[1][1])):
@@ -10,4 +11,4 @@ for n in range(len(root[1][1])):
 	root[1][1][n][0][0].attrib['value'] = root[1][1][n][0][0].attrib['value'].replace(']','')
 	root[1][1][n][0][0].attrib['value'] = root[1][1][n][0][0].attrib['value'].replace('"','')
 
-tree.write(path + 'modified.gexf',encoding='utf-8',xml_declaration=True)
+tree.write(path + 'modified.gexf', encoding="utf-8", xml_declaration=True)
