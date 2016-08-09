5207269387ebcf330172742da24e87a6edfa3004
Benjamin Cooper <bencooper222@gmail.com>
2016-08-09 09:52:43 -0500

    Altered to not include weird ns0 in text files

--- Python/xmlListformatter.py
+++ Python/xmlListformatter.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as et
 
 path = 'C:/Users/Yugan/Documents/GitHub/Leadership-Map-Website/Data/'
+et.register_namespace('', 'http://www.gexf.net/1.3')
 tree = et.parse(path + 'no name no zeros.gexf')
 root = tree.getroot()
 
@@ -9,4 +10,4 @@ for n in range(len(root[1][1])):
 	root[1][1][n][0][0].attrib['value'] = root[1][1][n][0][0].attrib['value'].replace(']','')
 	root[1][1][n][0][0].attrib['value'] = root[1][1][n][0][0].attrib['value'].replace('"','')
 
-tree.write(path + 'modified.gexf')
+tree.write(path + 'modified.gexf',encoding='utf-8',xml_declaration=True)
